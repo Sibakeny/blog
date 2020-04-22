ac75bbc0f3bdce909c304ea931429a86e2818609
yonetani <yonetani@ataglance.co.jp>
2020-04-22 20:39:54 +0900

    add article

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,7 +1,11 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body
+  attributes :id, :title, :body, :short_body
 
   has_many :categories, serializer: CategorySerializer do
     object.categories
+  end
+
+  def short_body
+    ActionController::Base.helpers.strip_tags(object.body)[0..100] + "......."
   end
 end
