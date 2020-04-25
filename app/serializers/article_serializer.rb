72e3b7c95a4a8d7cdd174d537be1841cdddbb762
yonetani <yonetani@ataglance.co.jp>
2020-04-25 19:44:21 +0900

    pagination

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,5 +1,5 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :short_body
+  attributes :id, :title, :body, :short_body, :created_at
 
   has_many :categories, serializer: CategorySerializer do
     object.categories
@@ -7,5 +7,9 @@ class ArticleSerializer < ActiveModel::Serializer
 
   def short_body
     ActionController::Base.helpers.strip_tags(object.body)[0..100] + "......."
+  end
+
+  def created_at
+    object.created_at.strftime("%Y年%-m月%-d日")
   end
 end
