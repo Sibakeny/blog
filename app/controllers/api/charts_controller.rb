095c69081b1f7a70a65b8503899e017086c223c3
yonetani <yonetani@ataglance.co.jp>
2020-04-29 17:05:40 +0900

    add limit and order

--- app/controllers/api/charts_controller.rb
+++ app/controllers/api/charts_controller.rb
@@ -2,8 +2,8 @@ class Api::ChartsController < ApplicationController
     def index
         article_values = Article.select("DATE_FORMAT(article_view_counters.created_at, '%Y-%m-%d') as time, count(*) as sum")
                                 .joins(:article_view_counters).group("DATE_FORMAT(article_view_counters.created_at, '%Y-%m-%d')")
+                                .order('time asc').limit(30)
 
         render json: article_values, each_serializer: ChartsSerializer
-        
     end
 end
