da03ecae90d63eafb988b9e620db6cd5d5a49f7b
yonetani <yonetani@ataglance.co.jp>
2020-05-02 23:39:24 +0900

    rubocop

--- spec/requests/api/articles_spec.rb
+++ spec/requests/api/articles_spec.rb
@@ -1,32 +1,34 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Articles', type: :request do
-    describe 'GET /articles' do
-        before do
-            create(:article, title: 'title1')
-            create(:article, title: 'title2')
-        end
-
-        it '記事一覧の取得ができること' do
-            get "/api/articles"
-            expect(response.status).to eq 200
-            json = JSON.parse(response.body)
-            expect(json['articles'].length).to eq 2
-            expect(json['articles'].first['title']).to eq 'title2'
-            expect(json['articles'].last['title']).to eq 'title1'
-        end
+  describe 'GET /articles' do
+    before do
+      create(:article, title: 'title1')
+      create(:article, title: 'title2')
     end
 
-    describe 'GET /articles/:id' do
-        before do
-            @article = create(:article, title: 'title1')
-        end
-
-        it '記事の情報が取得できること' do
-            get "/api/articles/#{@article.id}"
-            expect(response.status).to eq 200
-            json = JSON.parse(response.body)
-            expect(json['title']).to eq 'title1'
-        end
+    it '記事一覧の取得ができること' do
+      get '/api/articles'
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+      expect(json['articles'].length).to eq 2
+      expect(json['articles'].first['title']).to eq 'title2'
+      expect(json['articles'].last['title']).to eq 'title1'
     end
+  end
+
+  describe 'GET /articles/:id' do
+    before do
+      @article = create(:article, title: 'title1')
+    end
+
+    it '記事の情報が取得できること' do
+      get "/api/articles/#{@article.id}"
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+      expect(json['title']).to eq 'title1'
+    end
+  end
 end
