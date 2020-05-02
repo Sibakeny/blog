b80e7031b0c547db67d70a36f87dc4a7e9478744
yonetani <yonetani@ataglance.co.jp>
2020-05-02 15:39:32 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    before_action :set_current_user
+  before_action :set_current_user
 
-    def set_current_user
-        if !current_user
-            redirect_to session_path
-        end
+  def set_current_user
+    redirect_to session_path unless current_user
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user || User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        session[:user_id] = user.id
+        @current_user = user
+      end
     end
-    
-    def current_user
-        if (user_id = session[:user_id])
-            @current_user || User.find_by(id: user_id)
-        elsif (user_id = cookies.signed[:user_id])
-            user = User.find_by(id: user_id)
-            if user && user.authenticated?(cookies[:remember_token])
-                session[:user_id] = user.id
-                @current_user = user
-            end
-        end
-    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-    skip_before_action :set_current_user
+  skip_before_action :set_current_user
 
-    def show
+  def show; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+
+      redirect_to root_path
     end
+  end
 
-    def create
-        user = User.find_by(email: params[:email])
-        if user.authenticate(params[:password])
-            session[:user_id] = user.id
-            user.remember
-            cookies.permanent.signed[:user_id] = user.id
-            cookies.permanent[:remember_token] = user.remember_token
-
-            redirect_to root_path
-        end
-    end
-
-    def destroy
-        current_user.update_attribute(:remember_digest, nil)
-        session[:user_id] = nil
-        cookies.delete(:user_id)
-        cookies.delete(:remember_token)
-        redirect_to session_path
-    end
+  def destroy
+    current_user.update_attribute(:remember_digest, nil)
+    session[:user_id] = nil
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+    redirect_to session_path
+  end
 end
