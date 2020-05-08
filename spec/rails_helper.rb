2916f34d3ceef118ce13fa511a80c6858e394c94
yonetani <yonetani@ataglance.co.jp>
2020-05-08 22:09:07 +0900

    commit for ci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,7 +50,7 @@ RSpec.configure do |config|
   config.before(:each) do |example|
     if example.metadata[:type] == :system
       driven_by :selenium,
-        using: :chrome_headless,
+        using: :selenium_chrome_headless,
         screen_size: [1400, 1400]
     end
   end
