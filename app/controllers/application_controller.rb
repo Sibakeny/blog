1969d92c97f4350fd799c323efbbb89657265ae0
yonetani <yonetani@ataglance.co.jp>
2020-06-27 22:06:01 +0900

    authentication to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Authentication
+
   before_action :set_current_user
 
   def set_current_user
@@ -19,17 +21,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def login
-    session[:user_id] = @user.id
-    @user.remember
-    cookies.permanent.signed[:user_id] = @user.id
-    cookies.permanent[:remember_token] = @user.remember_token
-  end
-
-  def logout
-    current_user.update_attribute(:remember_digest, nil)
-    session[:user_id] = nil
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
 end
---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,17 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  def login
+    session[:user_id] = @user.id
+    @user.remember
+    cookies.permanent.signed[:user_id] = @user.id
+    cookies.permanent[:remember_token] = @user.remember_token
+  end
+
+  def logout
+    current_user.update_attribute(:remember_digest, nil)
+    session[:user_id] = nil
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+end
