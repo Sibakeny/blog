429c85662c45d4d26470263222d475040bf365c1
yonetani <yonetani@ataglance.co.jp>
2020-06-28 20:48:44 +0900

    i18n

--- app/controllers/article_view_counters/articles_controller.rb
+++ app/controllers/article_view_counters/articles_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ArticleViewCounters::ArticlesController < ApplicationController
+  add_breadcrumb 'HOME', :root_path
+  add_breadcrumb 'アクセスログ', :article_view_counters_path
+
   def show
-    add_breadcrumb 'HOME', root_path
-    add_breadcrumb 'アクセスログ', article_view_counters_path
     add_breadcrumb '記事のアクセスログ'
     @article = Article.find(params[:id])
     @chart_values = @article.article_chart_values_by_date
