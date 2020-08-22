939ead82e53e6930c93d6aa572993fa5ed92b22b
yonetani <yonetani@ataglance.co.jp>
2020-08-22 21:59:53 +0900

    add sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,3 +24,24 @@ import "../stylesheets/application";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+jQuery(function ($) {
+  $(".sidebar-dropdown > a").click(function () {
+    $(".sidebar-submenu").slideUp(200);
+    if ($(this).parent().hasClass("active")) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this).parent().removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this).next(".sidebar-submenu").slideDown(200);
+      $(this).parent().addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function () {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function () {
+    $(".page-wrapper").addClass("toggled");
+  });
+});
