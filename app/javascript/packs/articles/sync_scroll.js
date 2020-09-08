1e7c97f9194b48b4b358737400421044ceca38ab
yonetani <yonetani@ataglance.co.jp>
2020-09-08 23:41:48 +0900

    scroll sync

--- app/javascript/packs/articles/sync_scroll.js
+++ app/javascript/packs/articles/sync_scroll.js
@@ -2,8 +2,6 @@ var form = $('.article-body-form')
 var markdown = $('.article-body-markdown')
 
 form.scroll(function() {
-
-  markdown.scrollTop(form.scrollTop());//縦スクロールを連動させる。
+  markdown.scrollTop(form.scrollTop() * markdown.get(0).scrollHeight/(form.get(0).scrollHeight));//縦スクロールを連動させる。
   markdown.scrollLeft(form.scrollLeft());//横スクロールを連動させる。
-
 });
