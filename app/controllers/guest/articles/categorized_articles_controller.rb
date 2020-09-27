6ce315c6c9963ad089b324974989dc5c70be3904
yonetani <yonetani@ataglance.co.jp>
2020-09-27 18:40:15 +0900

    カテゴリ別の記事一覧の修正

--- app/controllers/guest/articles/categorized_articles_controller.rb
+++ app/controllers/guest/articles/categorized_articles_controller.rb
@@ -2,6 +2,6 @@ class Guest::Articles::CategorizedArticlesController < Guest::Base
 
   def index
     @category = Category.find(params[:category_id])
-    @articles = @category.articles
+    @articles = @category.articles.page(params[:page]).per(8)
   end
 end
