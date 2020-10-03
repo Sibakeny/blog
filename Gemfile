153475a9ddb4a11a64319c35d625e35f300144e7
yonetani <yonetani@ataglance.co.jp>
2020-10-03 15:41:51 +0900

    capistrano

--- Gemfile
+++ Gemfile
@@ -48,17 +48,24 @@ gem 'reverse_markdown'
 # 初期データ
 gem 'seed-fu'
 
+gem 'listen', '>= 3.0.5', '< 3.2'
+
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'capybara'
   gem 'rubocop'
   gem 'selenium-webdriver'
+
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
 end
 
 group :development do
   gem 'database_cleaner'
   gem 'factory_bot_rails'
-  gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'rspec-rails', '~> 4.0.0'
   gem 'spring'
   gem 'spring-commands-rspec'
--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -2,12 +2,12 @@
   $worker  = 2
   $timeout = 30
   $app_dir = "/var/www/sbkn_blog" #自分のアプリケーション名
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
+  $listen  = "#{app_path}/shared/tmp/sockets/unicorn.sock"
+  $pid     = "#{app_path}/shared/tmp/pids/unicorn.pid"
+  $std_log = "#{app_path}/shared/log/unicorn.stdout.log"
   # set config
   worker_processes  $worker
-  working_directory $app_dir
+  working_directory "#{app_path}/current"
   stderr_path $std_log
   stdout_path $std_log
   timeout $timeout
