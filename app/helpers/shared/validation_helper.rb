4049be26e074231265d283e1aa770310695d3a55
yonetani <sbkn0919@gmail.com>
2020-11-01 17:47:20 +0900

    remove debug code

--- app/helpers/shared/validation_helper.rb
+++ app/helpers/shared/validation_helper.rb
@@ -4,9 +4,6 @@ module Shared::ValidationHelper
   def form_validation_message(form_model, column)
     return unless form_model.errors.include?(column.to_sym)
 
-    p form_model
-    p form_model.errors
-
     content_tag(:div, class: 'd-flex justify-content-start') do
       concat content_tag(:p, form_model.errors.full_messages_for(column.to_sym).first, class: 'text-danger text-sm')
     end
