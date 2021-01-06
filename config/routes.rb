328e002b52881ba2cefeef29d64d3747038e29ee
yonetani <sbkn0919@gmail.com>
2021-01-06 21:38:15 +0900

    remove api routes

--- config/routes.rb
+++ config/routes.rb
@@ -57,35 +57,4 @@ Rails.application.routes.draw do
     end
   end
 
-  # API 使わなくなったけども消すのもちょっとなので一応残してる、、、
-  namespace :api do
-    resources :charts
-
-    resources :articles do
-      scope module: :articles do
-      end
-
-      collection do
-        scope module: :articles do
-        end
-      end
-    end
-
-    resources :article_methods, only: [] do
-      scope module: :article_methods do
-        resources :with_counters, only: [:index]
-        resources :searches, only: [:index]
-        resources :charts, only: [:index]
-      end
-    end
-
-    resources :category do
-      collection do
-        scope module: :category do
-          resources :filters, only: [:index]
-        end
-      end
-    end
-  end
-
 end
